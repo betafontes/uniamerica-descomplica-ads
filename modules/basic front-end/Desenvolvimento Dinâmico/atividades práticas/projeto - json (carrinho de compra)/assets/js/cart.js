15a296e2b05a222dd614027140e76127735aa666
Diogo Zura <diogozura@gmail.com>
2023-09-10 01:14:15 -0300

    Correção do bug em tela
    erro no fechando de Tag HTML , corrigido

--- modules/basic front-end/Desenvolvimento Dinâmico/atividades práticas/projeto - json (carrinho de compra)/assets/js/cart.js
+++ modules/basic front-end/Desenvolvimento Dinâmico/atividades práticas/projeto - json (carrinho de compra)/assets/js/cart.js
@@ -36,9 +36,9 @@ function atualizarCarrinho() {
 
   carrinho.forEach((produto, index) => {
     carrinhoHTML.innerHTML += 
-    `<div>
-           id='produto${index}'
-           class='d-flex flex-row justidy-content-between align-items-center pt-lg-12 pt-md-12 pt-2 pb-3 border-botton mobile'
+    `<div
+          id='produto${index}'
+           class='d-flex flex-row justidy-content-between align-items-center pt-lg-12 pt-md-12 pt-2 pb-3 border-botton mobile'>
         </div>
 
         <div class='d-flex flex-row align-items-center'>
@@ -64,21 +64,21 @@ function atualizarCarrinho() {
         </div>
 
         <div class='pl-md-3 pl-2'>
-           <span class='fa fa-minus-square text-secondary'>
-              onclick='removeItem(${index})'
+           <span class='fa fa-minus-square text-secondary'
+              onclick='removeItem(${index})'>
            </span>
 
            <span class='px-md-3 px-1' id='quantidade${index}'>
               ${produto.quantidade}
            </span>
 
-            <span class='fa fa-plus-square text-secondary'>
-              onclick='adicionarItem(${index})'
+            <span class='fa fa-plus-square text-secondary'
+              onclick='adicionarItem(${index})'>
            </span>
         </div>
 
         <div class='pl-md-0 pl-1'>
-           <b>R$ <span id='total${index}'>0</span></b>
+           <b>R$ <span id='total ${index}'>0</span></b>
         </div>
 
         <div class='close' onclick='removeProduto(${index})'>&times;</div>`;
@@ -86,7 +86,7 @@ function atualizarCarrinho() {
 }
 
 function adicionarItem(item) {
-  console.log(item);
+ 
   let qtd = document.getElementById('quantidade' + item);
   let produto = carrinho[item];
 
